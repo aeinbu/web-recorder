e67b7627b34a80465e8c2e580935fe7ee857e657
Arjan Einbu <arjan@einbu.no>
2018-06-23 16:55:17 +0200

    Responding to requests

---
+++ README.md
@@ -0,0 +1,24 @@
+# Web Recorder or Http Recorder
+A webserver that records any incoming requests.
+
+This utility helps examine incoming payloads. This can be usefull when setting up a webhooks receiver.
+
+This utility could also act as a replayer proxy to use a web resource is offline.
+
+Web Recorder starts up two different http endpoints, one for recording all requests, and one for examining the recordings, as well as administering the recorder.
+
+### v1
+- http endpoint(s)
+  - records request path, querystring, verbs, headers, post/payload etc
+  - can respond with predefined messages
+- WebUI to list and examine at recordings
+
+### v2
+- proxy
+  - records requests and their responses
+- replayer
+  - can replay responses to requests differing on different paths, querystring, post messages etc
+  - some customization of how to select a response to a requests (what to ignore etc)
+
+### v3
+- WebUI response and request editor
--- web/Startup.cs
+++ web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,26 +10,43 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace web
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+			app.Run(async (context) =>
+			{
+                var path = context.Request.Path.ToString();
+                var querystring = context.Request.QueryString;
+                var protocol = context.Request.Protocol;    // "HTTP/1.1"
+                var scheme = context.Request.Scheme;        // "https" or https"
+				var method = context.Request.Method;        // "GET", "POST", "PUT", "DELETE" etc
+				var headers = string.Concat(context.Request.Headers.Select(hdr => $"{hdr.Key}: {hdr.Value}\r\n"));
+				var body = await ResolveStreamToString(context.Request.Body);
+
+				var response = $"This was the request made:\r\n\r\n{method} {scheme}://servername:port{path}{querystring} {protocol}\r\n{headers}\r\n{body}";
+				await context.Response.WriteAsync(response);
+			});
+		}
+
+		private async Task<string> ResolveStreamToString(Stream stream)
+		{
+			using (var sr = new StreamReader(stream))
+			{
+                return await sr.ReadToEndAsync();
+			}
+		}
+	}
 }
