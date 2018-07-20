8a14c6b90ff30349605eeae458be9ec8e43e43b7
Arjan Einbu <arjan@einbu.no>
2018-07-21 01:36:27 +0200

    Work in progress

--- middleware/HttpProxyMiddleware.cs
+++ middleware/HttpProxyMiddleware.cs
@@ -1,38 +1,69 @@
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 
 namespace Middleware
 {
 	public class HttpProxyMiddleware
 	{
 		private readonly RequestDelegate _next;
+		private readonly ILogger<HttpProxyMiddleware> _logger;
 
-		public HttpProxyMiddleware(RequestDelegate next)
+		public HttpProxyMiddleware(RequestDelegate next, ILogger<HttpProxyMiddleware> logger)
 		{
 			_next = next;
+			_logger = logger;
 		}
 		public async Task InvokeAsync(HttpContext context)
 		{
-			// Make a request to requested server
-			var client = new HttpClient();
-			var requestMessage = CreateRequestMessage(context.Request);
-			var responseMessage = await client.SendAsync(requestMessage);
+			if (context.Request.Headers.ContainsKey("Host"))
+			{
+				// Make a request to requested server
+				var client = new HttpClient();
+				var requestMessage = CreateRequestMessage(context.Request);
+				var responseMessage = await client.SendAsync(requestMessage);
 
-			// Return the responseMessage as a response
-			await responseMessage.Content.CopyToAsync(context.Response.Body);
+				// Return the responseMessage as a response
 
-			//TODO: Copy all other response headers
+				// responseMessage.Headers.Add("Content-Type", "text/html");
+				await responseMessage.Content.CopyToAsync(context.Response.Body);
+				// using (_logger.BeginScope("*** Response headers"))
+				// {
+				// 	foreach (var hdr in responseMessage.Headers)
+				// 	{
+				// 		_logger.LogDebug($"{hdr.Key}: {hdr.Value}");
+				// 		context.Response.Headers.Add(hdr.Key, new StringValues(hdr.Value.ToArray()));
+				// 	}
+				// }
+			}
+			else
+			{
+				// Current request is not a proxy request, since it misses the "Host" header
+				await _next(context);
+			}
 		}
 
 		private HttpRequestMessage CreateRequestMessage(HttpRequest request)
 		{
 			var host = $"{request.Scheme}://{request.Headers["Host"]}";
 			var method = ResolveHttpMethod(request.Method);
-			var requestMessage = new HttpRequestMessage(method, host);
+			var headers = request.Headers.Where(hdr => hdr.Key != "Host" && hdr.Key != "Content-type");
 
-			//TODO: copy all other request headers
+			var requestMessage = new HttpRequestMessage(method, host);
+			// requestMessage.Headers.Add("Accept", new[] { "text/html" });
+
+			// using (_logger.BeginScope("*** Request headers"))
+			// {
+			// 	foreach (var hdr in headers)
+			// 	{
+			// 		_logger.LogDebug($"{hdr.Key}: {hdr.Value}");
+			// 		requestMessage.Headers.Add(hdr.Key, hdr.Value.AsEnumerable());
+			// 	}
+			// }
 
 			return requestMessage;
 		}
